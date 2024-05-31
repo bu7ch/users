e9202f8a4cf43ce40916252b3b5db2db0c3bbb45
Rodolphe <dutchr@live.fr>
2024-05-31 13:58:20 +0200

    :sparkles: Changing adaptater for connecting DB w/ mySQL

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,22 +1,28 @@
-const User = require("../models/userModel");
+
 
 const allUsers = async (req, res) => {
+  const db = req.app.get("db");
   try {
-    const users = await User.find();
-    res.json(users);
-  } catch (error) {
-    res.json(error);
+    const [rows] = await db.query("SELECT * FROM users");
+    res.json(rows);
+  } catch (err) {
+    console.error("Error fetching users:", err);
+    res.status(500).json({ error: "Failed to fetch users" });
   }
 };
 
 const createUser = async (req, res) => {
+  const db = req.app.get("db");
   const { name, email } = req.body;
   try {
-    const user = new User({ name, email });
-    await user.save();
-    res.json({message: 'User created successfully', user})
-  } catch (error) {
-    res.json(error)
+    const [result] = await db.query(
+      "INSERT INTO users (name, email) VALUES (?, ?)",
+      [name, email]
+    );
+    res.json({ message: "User created successfully", newUser: { id: result.insertId, name, email } });
+  } catch (err) {
+    console.error("Error creating user:", err);
+    res.status(500).json({ error: "Failed to create user" });
   }
 };
 
--- index.js
+++ index.js
@@ -1,21 +1,35 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
-const port = 5678
-const userRouter = require('./routes/userRoute');
+const port = 5678;
+const userRouter = require("./routes/userRoute");
 
-main().catch(err => console.log(err));
+const mysql = require("mysql2/promise");
+const dbPromise = () => {
+  return mysql.createConnection({
+    host: "127.0.0.1",
+    user: "root",
+    password: "root",
+    database: "user_db",
+    connector: "mysql",
+    socketPath: "/Applications/MAMP/tmp/mysql/mysql.sock",
+  });
+};
 
-async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/users_db');
-  console.log(`[DATABASE] MongoDB connected !`)
-}
+dbPromise()
+  .then((connection) => {
+    console.log("Connected to MySQL");
+    app.set("db", connection);
+  })
+  .catch((err) => {
+    console.error("Error connecting to MySQL:", err);
+  });
 
+app.use(express.json());
+app.get("/", (req, res) => {
+  res.json("Welcome on API for users");
+});
+app.use("/api/users", userRouter);
 
-app.use(express.json())
-app.get('/', (req, res) => {
-  res.json('Welcome on API fors users')
-})
- app.use('/api/users', userRouter)
-
-app.listen(port, ()=> console.log(`[SERVER] is runnning on http://localhost:${port}`))
+app.listen(port, () =>
+  console.log(`[SERVER] is running on http://localhost:${port}`)
+);
